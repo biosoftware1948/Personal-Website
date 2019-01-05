535961bb628ef2beea50d92b626ac23de2a84057
mfin23 <mfindlay@scu.edu>
2019-01-04 17:09:41 -0800

    Removed ssl

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -16,19 +16,5 @@ def show_resume():
 def handle_robots():
     return send_from_directory("static", "robots.txt")
 
-@app.route("/.well-known/acme-challenge/<challenge>")
-def letsencrypt_check(challenge):
-    challenge_dictionary = {
-            "AqHWwPuqUyy9ghCTbkz-OQAS5E2X2pLseFipXtX6W6Y":
-            "AqHWwPuqUyy9ghCTbkz-OQAS5E2X2pLseFipXtX6W6Y.lWwPcj0C7stKhHWAa7b8EAJSvK7HzgHWKtLRy1kKSfk",
-            "6cMPtuDn8KKs3amdeBGcYgqjj3PcXjjRVsrqajyBKFY":
-            "6cMPtuDn8KKs3amdeBGcYgqjj3PcXjjRVsrqajyBKFY.lWwPcj0C7stKhHWAa7b8EAJSvK7HzgHWKtLRy1kKSfk"
-            }
-    return Response(challenge_dictionary[challenge], mimetype='text/plain')
-
-
 if __name__ == "__main__":
-    context = SSL.Context(SSL.SSLv23_METHOD)
-    context.use_privatekey_file('etc/letsencrypt/live/mattfindlay.com/privkey.pem')
-    context.use_certificate_file('/etc/letsencrypt/live/mattfindlay.com/fullchain.pem')
-    app.run(ssl_context=(context))
+    app.run()
